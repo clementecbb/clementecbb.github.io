88557d0f5cb196e72944a30b85115e63f1875586
Clemente Barros <clemente.barros.b@gmail.com>
2025-03-17 14:50:08 -0300

    changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,8 +3,8 @@ export default function Hero() {
         <div className="container mx-auto">
             <div className="flex justify-center items-center h-screen">
                 <div className="text-center">
-                    <h1 className="text-4xl font-bold">Hello, I'm Clemente Barros</h1>
-                    <p className="text-2xl">I'm a Software Developer</p>
+                    <h1 className="text-4xl font-bold">Hello, I&lsquo;m Clemente Barros</h1>
+                    <p className="text-2xl">I&lsquo;m a Software Developer</p>
                 </div>
             </div>
         </div>
